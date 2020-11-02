62a18a8d9a9fc21c84184c984bd47a522379dea6
Michael Famurewa <myfamurewa@gmail.com>
2020-11-02 18:28:18 -0500

    cleaning up

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import logger from 'redux-logger';
 import './index.css';
 import App from './App';
-// import {reducer} from "./reducers"
 import reportWebVitals from './reportWebVitals';
 
-const store = createStore( applyMiddleware(thunk, logger))
 
 ReactDOM.render(
-  <Provider store={store}>
-  <Router>
-    <App />
-  </Router>
-  </Provider>,
+    <App />,
   document.getElementById('root')
 );
 
